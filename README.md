7fe64acc6f1274d21fa0edc0c8712d95021a9a9a
Artem Tyurin <spacevillain@ya.ru>
2012-02-27 13:02:51 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+### Instagram twitter fix (Chrome extenstion)
+
+Simple fix if instagram pictures are not loading for you in the twitter stream.
+
+Build it yourself from ./src (Tools > Extensions > Developer Mode > Pack extension) or use the one in ./bin.
---
+++ src/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+  var api = "http://api.instagram.com/oembed?url=";
+  
+  $('.tweet').live("mousedown", function(event) { // 'click' is not working, no idea why
+    if (event.which != 1) return;
+    
+    var that = this,
+        link = $(this).find('.js-tweet-text').find('a[data-expanded-url]'),
+        url = link.attr('data-expanded-url');
+
+    if (link.length > 0 && url && url.indexOf('http://instagr.am') != -1) {
+      $.getJSON("http://api.instagram.com/oembed?url=" + url, function(data) {
+        (function findContainer() {
+          var container = $(that).find('.js-tweet-media-container');
+
+          if (container.length > 0) {
+            var img = $('<img>', {src: data.url});
+            
+            setTimeout(function() {
+              container.html(img.css({'max-height': '100%', 'max-width': '100%'}));
+            }, 300);
+
+          } else {
+            setTimeout(findContainer, 1000);
+          }
+        })();
+      });
+    }
+  });
+});
